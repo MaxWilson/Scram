c56c345f8fe39bad37386cfbc52845de2450e9f3
Max Wilson <maxw@microsoft.com>
2016-11-16 15:53:46 -0800

    Add lava animation

--- Behavior.fs
+++ Behavior.fs
@@ -39,22 +39,39 @@ let rec addUnicorn (container: Container) =
     p.interactive <- true
     container.addChild(p) |> ignore
 
-let addText (stage: Container) msg =
+let addText (stage: Container) msg color1 color2 =
     let text =
         Fable.Import.PIXI.Text(
             msg,
             [
             TextStyle.Font "bold italic 24px Arial"
-            TextStyle.Fill (U2.Case1 "#3e1707")
+            TextStyle.Fill (U2.Case1 color1)
             TextStyle.Align "center"
-            TextStyle.Stroke (U2.Case1 "#a4410e")
+            TextStyle.Stroke (U2.Case1 color2)
             TextStyle.StrokeThickness 7.
             ], position=Point(randomRational 500., randomRational 500.))
     stage.addChild(text)
     |> ignore
 
+let makeAnimation(url, size: int, spritecoords: (int * int) list) =
+    let texture = Texture.fromImage(url)
+    let frames = spritecoords |> List.map (fun (x,y) -> Texture(texture, Rectangle(float x, float y, float size, float size))) |> ResizeArray
+    let movie = PIXI.extras.MovieClip(frames)
+    movie.gotoAndPlay(unbox (random frames.Count - 1))
+    movie
+
+let lava (stage: Container) x y =
+    let lava = makeAnimation("Lava.png", 128, [2,114;132,114;262,114;392,114;522,114;652,114;782,114;2,244;132,244;262,244;392,244])
+    lava.position <- Point(x, y)
+    lava.scale <- Point(0.5, 0.5)
+    lava.animationSpeed <- 0.2
+    stage.addChild(lava) |> ignore
+
 let onStart stage =
-    addText stage "Hello"
+    addText stage "Hello" "blue" "red"
+    addText stage "World" "orange" "purple"
+    lava stage 330. 220.
+    lava stage (330.+64.) 220.
     ()
 
 let everySecond stage =
